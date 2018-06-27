8e798ff4f91f008d645c04641147979dc19ce482
Joaquin Frank Jonislla <joaquin@joaquin.joaquin.sebastian>
2018-06-27 11:00:34 -0500

    serializador

---
+++ application-service/src/main/java/banking/accounts/application/dto/RequestBankAccountDto.java
@@ -0,0 +1,63 @@
+package banking.accounts.application.dto;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import banking.accounts.application.dto.deserializer.RequestBankAccountDtoDeserializer;
+import banking.common.application.dto.RequestBaseDto;
+import banking.common.application.enumeration.RequestBodyType;
+
+@JsonDeserialize(using = RequestBankAccountDtoDeserializer.class)
+public class RequestBankAccountDto extends RequestBaseDto {
+	private long id;
+	private String number;
+	private BigDecimal balance;
+	private boolean isLocked;
+	
+	private long customerId;
+	
+	private RequestBodyType requestBodyType;
+	
+	
+
+	public RequestBankAccountDto(long id, String number, BigDecimal balance, boolean isLocked, long customerId, RequestBodyType requestBodyType) {
+		super();
+		this.id = id;
+		this.number = number;
+		this.balance = balance;
+		this.isLocked = isLocked;
+		this.customerId = customerId;
+		this.requestBodyType = requestBodyType;
+	}
+
+	public RequestBankAccountDto(RequestBodyType requestBodyType) {
+		this.requestBodyType = requestBodyType;
+	}
+
+	public RequestBodyType getRequestBodyType() {
+		return requestBodyType;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public boolean isLocked() {
+		return isLocked;
+	}
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	
+}
---
+++ application-service/src/main/java/banking/accounts/application/dto/deserializer/RequestBankAccountDtoDeserializer.java
@@ -0,0 +1,46 @@
+package banking.accounts.application.dto.deserializer;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import banking.accounts.application.dto.RequestBankAccountDto;
+import banking.common.application.enumeration.RequestBodyType;
+
+public class RequestBankAccountDtoDeserializer extends JsonDeserializer<RequestBankAccountDto> {
+	
+	Logger logger = LoggerFactory.getLogger(RequestBankAccountDtoDeserializer.class);
+	
+	@Override
+	public RequestBankAccountDto deserialize(JsonParser jsonParser, DeserializationContext ctxt)
+			throws IOException, JsonProcessingException {
+		RequestBankAccountDto requestBankTransferDto = null;
+		try {
+			
+			
+			
+    		ObjectCodec objectCodec = jsonParser.getCodec();
+            JsonNode node = objectCodec.readTree(jsonParser);
+            
+            long id = new Long(node.get("id").asText());
+            long customerId = new Long(node.get("customerId").asText());
+            String accountNumber = node.get("number").asText();
+            BigDecimal balance = new BigDecimal(node.get("balance").asText());
+            boolean isLocked = new Boolean(node.get("isLocked").asText());
+            
+            requestBankTransferDto = new RequestBankAccountDto(id, accountNumber, balance, isLocked, customerId, RequestBodyType.VALID);
+    	} catch(Exception ex) {
+    		requestBankTransferDto = new RequestBankAccountDto(RequestBodyType.INVALID);
+    	}
+        return requestBankTransferDto;
+	}	
+}
