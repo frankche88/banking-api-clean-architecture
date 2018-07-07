85d1db74883084b3e988efd3e6b06f3c991b3e88
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2018-07-07 11:27:38 -0500

    migrations

--- application-spring-boot/src/main/resources/db/migration/V1_3__insert_customer.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO customer(first_name, last_name, document_number, active) VALUES('Juan', 'Perez', 28308796, 1);
--- application-spring-boot/src/main/resources/db/migration/V1_4__insert_bank_accounts.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO bank_account(number, balance, locked, customer_id) VALUES('123-456-001', 1500, 0, 2);
-INSERT INTO bank_account(number, balance, locked, customer_id) VALUES('123-456-002', 1800, 0, 2);
