aadb26b2fcf5f9e7e223ef65ca05af5764eac9d6
Frank Jonislla <frank.che88@gmail.com>
2018-07-12 04:29:58 +0000

    correccion de retorno de error

--- application-spring-boot/src/main/java/banking/common/api/controller/ResponseHandler.java
+++ application-spring-boot/src/main/java/banking/common/api/controller/ResponseHandler.java
@@ -1,8 +1,5 @@
 package banking.common.api.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,12 +8,12 @@ import org.springframework.stereotype.Component;
 
 import banking.common.application.dto.ErrorDto;
 import banking.common.application.dto.ResponseDto;
-import banking.common.application.dto.ResponseErrorDto;
 import banking.common.application.dto.ResponseOkCommandDto;
 
 @Component()
 public class ResponseHandler {
 	private Logger LOGGER = LoggerFactory.getLogger(ResponseHandler.class);
+	
 	public ResponseEntity<Object> getOkCommandResponse(String message) {
 		ResponseDto responseDto = new ResponseDto();
 		ResponseOkCommandDto responseOkCommandDto = new ResponseOkCommandDto();
@@ -28,27 +25,12 @@ public class ResponseHandler {
 	
 	public ResponseEntity<Object> getAppCustomErrorResponse(String errorMessages)
     {
-		ResponseDto responseDto = new ResponseDto();
-		String[] errors = errorMessages.split(",");
-		List<ErrorDto> errorsDto = new ArrayList<ErrorDto>();
-        for (String error : errors) {
-            errorsDto.add(new ErrorDto(error));
-        }
-        ResponseErrorDto responseErrorDto = new ResponseErrorDto(errorsDto);
-        responseErrorDto.setHttpStatus(HttpStatus.BAD_REQUEST.value());
-        responseDto.setResponse(responseErrorDto);
-        return new ResponseEntity<Object>(responseDto.getResponse(), HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<Object>(new ErrorDto(HttpStatus.BAD_REQUEST.value(), errorMessages), HttpStatus.BAD_REQUEST);
     }
 	
 	public ResponseEntity<Object> getAppExceptionResponse(Exception ex)
     {
-		LOGGER.error("Error",ex);	
-		ResponseDto responseDto = new ResponseDto();
-		List<ErrorDto> errorsDto = new ArrayList<ErrorDto>();
-        errorsDto.add(new ErrorDto("Server error"));
-        ResponseErrorDto responseErrorDto = new ResponseErrorDto(errorsDto);
-        responseErrorDto.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        responseDto.setResponse(responseErrorDto);
-        return new ResponseEntity<Object>(responseDto.getResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+		LOGGER.error("Error",ex);
+        return new ResponseEntity<Object>(new ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Server error"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
